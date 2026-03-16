da856e1245feb4c230bcda89614aaea26e2c153c
Snapps <30825475+Snappsu@users.noreply.github.com>
2026-03-15 18:20:18 -0700

    Update entrypoint.sh

--- setup/nginx/entrypoint.sh
+++ setup/nginx/entrypoint.sh
@@ -1,22 +1,24 @@
+  GNU nano 7.2                                                                                 setup/nginx/entrypoint.sh *                                                                                         
 #!/bin/sh
 
 if [ "$USE_CERTBOT" == "yes" ]
 then
-	echo "Activating certbot..."
-	mkdir -p /var/www/certbot
+        echo "Activating certbot..."
+        mkdir -p /var/www/certbot
 
-	$(while :; do sh /opt/certbot.sh; sleep 12h; done;) &
+        $(while :; do sh /opt/certbot.sh; sleep 12h; done;) &
 
-	$(while inotifywait -e close_write /usr/share/nginx/certificates; do nginx -s reload; done) &
+        $(while inotifywait -e close_write /usr/share/nginx/certificates; do nginx -s reload; done) &
 fi
 
 apk add git
+rm -rf /var/www/public_html/*
 mkdir temp
 git clone -b "$SITE_GIT_BRANCH" "$SITE_GIT" temp
 cd temp
 rm -rf .git
 cd ..
-mv temp/* /var/www/public_html/
-rm -rf temp
+cp -R temp/* /var/www/public_html/
+rm -rf temp/
 
 exec nginx -g "daemon off;"
